6995d922c743fe86c7401efce9ec2a95e2f7250b
Antonio <tonehonk@gmail.com>
2025-01-06 15:03:38 +0200

    Add absolute and relative path logic

--- main.hs
+++ main.hs
@@ -10,3 +10,39 @@ pwd :: [String] -> String
 pwd currentDirectory = concatMap (++ "/") (init currentDirectory) ++ last currentDirectory
 -- >>> pwd currentDirectory
 -- "root/first/second/third"
+
+
+splitPathToTokens:: String -> [String]
+splitPathToTokens [] = []
+splitPathToTokens path = takeWhile (/= '/') path : splitPathToTokens ( dropWhile (== '/') (dropWhile (/= '/') path))
+
+-- >>> splitPathToTokens "../test1/../test2"
+-- ["..","test1","..","test2"]
+
+isAbsolutePath :: String -> Bool
+isAbsolutePath path = head path == '/'
+
+createQueryPath :: String -> [String]
+createQueryPath path
+    | isAbsolutePath path = splitPathToTokens path
+    | otherwise             = currentDirectory ++ splitPathToTokens path
+
+
+parsePath :: [String] -> [String] -> [String]
+parsePath [] tmp = tmp
+parsePath path tmp
+    | length tmp == 1 && head path == ".." = error "Invalid path"
+    | head path == ".."                    = parsePath (tail path) (init tmp)
+    | otherwise                           = parsePath (tail path)  (tmp ++ [head path])
+
+-- >>> parsePath (createQueryPath "../hi") []
+-- ["root","first","second","hi"]
+
+-- >>> parsePath (createQueryPath "/misho/../pesho") []
+-- ["","pesho"]
+
+-- >>> parsePath (createQueryPath "test2/test3/../../../test4") []
+-- ["root","first","second","test4"]
+
+-- >>> parsePath (createQueryPath "/tmp1/../../tmp2") []
+-- Invalid path
